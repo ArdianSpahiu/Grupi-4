645c29133b9f7a731321e6dad668fb226749916d
Denisa Isufi <denisa.isufi@student.uni-pr.edu>
2025-02-14 16:26:45 +0100

    krijimi i funksionit shto llogari

--- SistemiBankar.cpp
+++ SistemiBankar.cpp
@@ -6,8 +6,30 @@ string emrat[maxLlogari];
 int numratLlogarive[maxLlogari];
 double balanca[maxLlogari];
 int llogariTotale = 0;
+void krijollogari()     //Lejon perdoruesit qe te krijojne nje llogari te re
+{
+
+
+	if (llogariTotale>=maxLlogari)
+	{
+		cout << "Sistemi nuk pranon me shume perdorues tani per tani!" << endl;
+		return;
+	}
+	cout << "Jep Emrin e llogarise se perdoruesit: ";
+	cin >> emrat[llogariTotale];
+	cout << "Jep numrin e llogarise: ";
+	cin >> numratLlogarive[llogariTotale];
+	balanca[llogariTotale++] = 0;
+	cout << "Llogaria u krijua me sukses!" << endl;
+
+}
+
+
+
+
 int main()
 {
+
 	return 0;
 }
 

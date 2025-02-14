1357d53c18806e2acca4878f40032890c7859c8e
ArdianSpahiu <ardian.spahiu2@student.uni-pr.edu>
2025-02-14 15:46:03 +0100

    Struktura

--- SistemiBankar.cpp
+++ SistemiBankar.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <ioamanip>
+using namepsace std;
+const int maxLlogari = 100;
+string emrat[maxLlogari];
+int numratLlogarive[maxLlogari];
+double balanca[maxLlogari];
+int llogariTotale = 0;
+int main()
+{
+	return 0;
+}
 
